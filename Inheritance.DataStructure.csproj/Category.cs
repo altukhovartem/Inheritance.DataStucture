daef8ff4d2ec8ee10d6f4f99882729d94fb55ce8
aaltukhov <aaltukhov@acumatica.com>
2018-01-12 20:23:04 +0300

    second commit

--- Inheritance.DataStructure.csproj/Category.cs
+++ Inheritance.DataStructure.csproj/Category.cs
@@ -6,5 +6,29 @@ using System.Threading.Tasks;
 
 namespace Inheritance.DataStructure
 {
-    
+    class Category
+    {
+        public string CurrentBukva { get; set; }
+        public MessageType CurrentMessageType { get; set; }
+        public MessageTopic CurrentMessageTopic { get; set; }
+
+        public Category(string bukva, MessageType messageType, MessageTopic messageTopic)
+        {
+            CurrentBukva = bukva;
+            CurrentMessageType = messageType;
+            CurrentMessageTopic = messageTopic;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Category CategoryToCompare = obj as Category;
+            if (CategoryToCompare == null)
+            {
+                throw new Exception();
+            }
+
+
+        }
+    }
+
 }
