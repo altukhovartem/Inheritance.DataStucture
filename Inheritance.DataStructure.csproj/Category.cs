3a34aa971711009fecbb95f040a40f8c6cf39721
aaltukhov <aaltukhov@acumatica.com>
2018-01-16 19:08:50 +0300

    implemented compareTo method and overrided tostring

--- Inheritance.DataStructure.csproj/Category.cs
+++ Inheritance.DataStructure.csproj/Category.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Inheritance.DataStructure
 {
-    class Category
+    class Category: IComparable
     {
         public string CurrentTitle { get; set; }
         public MessageType CurrentMessageType { get; set; }
@@ -30,7 +30,36 @@ namespace Inheritance.DataStructure
                 return true;
             else
                 return false;
+        }
 
+        public int CompareTo(object obj)
+        {
+            Category CategoryToCompare = obj as Category;
+            if (CategoryToCompare == null)
+            {
+                throw new Exception("Can not compare objects of different types");
+            }
+
+            if (string.Compare(this.CurrentTitle, CategoryToCompare.CurrentTitle) == 1)
+                return 1;
+            else if (string.Compare(this.CurrentTitle, CategoryToCompare.CurrentTitle) == -1)
+                return -1;
+            else if (this.CurrentMessageType.CompareTo(CategoryToCompare.CurrentMessageType) == 1)
+                return 1;
+            else if (this.CurrentMessageType.CompareTo(CategoryToCompare.CurrentMessageType) == -1)
+                return -1;
+            else if (this.CurrentMessageTopic.CompareTo(CategoryToCompare.CurrentMessageTopic) == 1)
+                return 1;
+            else if (this.CurrentMessageTopic.CompareTo(CategoryToCompare.CurrentMessageTopic) == -1)
+                return -1;
+            else
+                return 0;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}.{2}", CurrentTitle, CurrentMessageType, CurrentMessageTopic);
         }
     }
 
